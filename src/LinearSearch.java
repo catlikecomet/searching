d8c216195e4c71d2f9ea2db23ef91c0d2cf8b1ec
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-02-07 10:42:00 +0000

    liner list complete woop woop

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,3 +1,13 @@
+import java.util.List;
+
 public class LinearSearch {
 
+    public String linearSearch(List<String> list, String word) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                return word;
+            }
+        }
+        return "word not in list";
+    }
 }
--- src/main.java
+++ src/main.java
@@ -5,7 +5,8 @@ public class main {
 
     public static void main (String[] args) throws IOException {
         List list =ReadFile.readFile("/Users/cmummery/Training/words.txt");
-
         System.out.println(list);
+        LinearSearch search = new LinearSearch();
+        System.out.println(search.linearSearch(list, "spurter"));
     }
 }
