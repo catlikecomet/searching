facb3120f76e5ec5573359684668fb16e92651a1
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-02-07 16:22:35 +0000

    changing middle of the list so it changes depending on if the word has been found or not

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch {
+    //Keep splitting in half until said word is found
+    public String binarySearch(List<String> list, String word) {
+        int firstIndex = 0;
+        int lastIndex = list.size();
+        int middleOfList = lastIndex - firstIndex / 2;
+
+        if (list.get(middleOfList).equals(word)) {
+            return word;
+        }
+
+        for (int i = 0; i < middleOfList; i++) {
+
+        }
+
+        return word + " is not in the list";
+    }
 }
