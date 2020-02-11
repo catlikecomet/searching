9d8de12f1f3384fa187a5f7fbc001ea4a3745a1b
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-02-11 10:40:06 +0000

    morgan showing me how to print  every item in  the list because i am silly

--- src/main.java
+++ src/main.java
@@ -4,15 +4,17 @@ import java.util.List;
 public class main {
 
     public static void main (String[] args) throws IOException {
-        List list =ReadFile.readFile("/Users/cmummery/Training/words.txt");
-        System.out.println(list);
+        List<String> list = ReadFile.readFile("/Users/cmummery/Training/words.txt");
+        for(String item: list){
+            System.out.println(item);
+        }
         LinearSearch search = new LinearSearch();
         BinarySearch otherSearch = new BinarySearch();
         System.out.println("Linear Search: ");
         System.out.println(search.linearSearch(list, "spurter"));
         System.out.println(search.linearSearch(list, "qwhjfhb"));
         System.out.println("Binary Search: ");
-        System.out.println(otherSearch.binarySearch(list, "spurter"));
+        System.out.println(otherSearch.binarySearch(list, "abaco"));
         System.out.println(otherSearch.binarySearch(list, "sqdfqfw"));
     }
 }
